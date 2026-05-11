1a7ea04970d48ae45d8e9d0574d24e873e16f4cf
bagasadit123 <bagasadit430@gmail.com>
2026-05-11 18:48:42 +0700

    membuat constructor dan destructor serta deklarasi prosedur

--- ibu.h
+++ ibu.h
@@ -9,4 +9,17 @@ public:
     //deklarasi member variabel pada class ibu
     string nama;
     vector<anak*> daftar_anak;
-}
+
+    //membuat constructor dan destructor pada class ibu
+    Ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    };
+
+    ~Ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    };
+
+    //deklarasi prosedur tambahAnak() dan cetakAnak()
+    void tambahAnak(anak* pAnak);
+    void cetakAnak();
+};
