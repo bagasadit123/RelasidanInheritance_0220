db2b99aa78dd594e08f16416232ca21dbf02a8fb
bagasadit123 <bagasadit430@gmail.com>
2026-05-11 18:57:11 +0700

    menyertakan file header komposisi

--- komposisi.cpp
+++ komposisi.cpp
@@ -5,12 +5,3 @@ using namespace std;
 //menyertakan file header jantung.h dan manusia.h
 #include "jantung.h"
 #include "manusia.h"
-
-int main()
-{
-    //deklarasi dan pemberian nilai object pointer varManusia
-    Manusia* varManusia = new Manusia("jono");
-    //mendelete pointer varManusia dari memori
-    delete varManusia;
-    return 0;
-}
